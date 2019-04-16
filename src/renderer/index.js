b4a00298254b79d56717892cc49749fa8a1d7a91
Patrick Laplante <laplante.patrick@gmail.com>
2019-04-16 09:00:49 -0400

    Got theming working and sass hooked up.

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,10 +1,38 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom';
+
+// class MyComponent extends React.Component {
+// 	render() {
+// 	  return <h1>Hello {this.props.message}!</h1>;
+// 	}
+// }
+  
+// ReactDOM.render(<MyComponent message="World from pat hola" />, document.getElementById('app'));
+
 import React from 'react'
 import ReactDOM from 'react-dom';
+import { Card, CardTitle, CardText, CardActions, Button, RaisedButton } from 'react-md'
 
-class MyComponent extends React.Component {
-	render() {
-	  return <h1>Hello {this.props.message}!</h1>;
-	}
+import './styles.scss'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="md-grid">
+        <Card className="md-cell">
+          <CardTitle title="Hello, World!" />
+          <h5>Theme Examples</h5>
+          {/* <CardText primary>
+            Lorem ipsum... pretend more ...
+          </CardText> */}
+          <CardActions>
+            <Button raised primary>Action 1</Button>
+            <Button flat secondary>Action 2</Button>
+          </CardActions>
+        </Card>
+      </div>
+    );
+  }
 }
-  
-ReactDOM.render(<MyComponent message="World from pat hola" />, document.getElementById('app'));
+
+ReactDOM.render( <App />, document.getElementById('app'));
