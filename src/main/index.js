d24ce515c8f3f053e64f52e1715a8123c8312c97
Patrick Laplante <laplante.patrick@gmail.com>
2019-04-23 13:24:50 -0400

    Messing around with understanding blueprintjs layout

--- src/main/index.js
+++ src/main/index.js
@@ -3,14 +3,43 @@
 import { app, BrowserWindow } from 'electron'
 import * as path from 'path'
 import { format as formatUrl } from 'url'
+import Store from '../common/store.js'
 
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
 // global reference to mainWindow (necessary to prevent window from being garbage collected)
 let mainWindow
 
+// Work around when running in dev mode. otherwise the name is always Electron
+// and the appdata folder is also Electron.
+const appName = 'Skaffe';
+app.setName(appName);
+const appData = app.getPath('appData');
+app.setPath('userData', path.join(appData, appName));
+
+const store = new Store({
+  // We'll call our data file 'user-preferences'
+  configName: 'user-preferences',
+  defaults: {
+    // 800x600 is the default size of our window
+    windowBounds: { width: 800, height: 600 }
+  }
+});
+
 function createMainWindow() {
-  const window = new BrowserWindow()
+
+  let { width, height } = store.get('windowBounds');
+  const window = new BrowserWindow({ width, height })
+
+  // The BrowserWindow class extends the node.js core EventEmitter class, so we use that API
+  // to listen to events on the BrowserWindow. The resize event is emitted when the window size changes.
+  window.on('resize', () => {
+    // The event doesn't pass us the window size, so we call the `getBounds` method which returns an object with
+    // the height, width, and x and y coordinates.
+    let { width, height } = window.getBounds();
+    // Now that we have them, save them using the `set` method.
+    store.set('windowBounds', { width, height });
+  });
 
   if (isDevelopment) {
     //window.webContents.openDevTools()
--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,61 +1,84 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom';
-
-// class MyComponent extends React.Component {
-// 	render() {
-// 	  return <h1>Hello {this.props.message}!</h1>;
-// 	}
-// }
-  
-// ReactDOM.render(<MyComponent message="World from pat hola" />, document.getElementById('app'));
-
-// import React from 'react'
-// import ReactDOM from 'react-dom';
-// import { Card, CardTitle, CardText, CardActions, Button, RaisedButton } from 'react-md'
-
-// import './styles.scss'
-
-// class App extends React.Component {
-//   render() {
-//     return (
-//       <div className="md-grid">
-//         <Card className="md-cell">
-//           <CardTitle title="Hello, World!" />
-//           <h5>Theme Examples</h5>
-//           {/* <CardText primary>
-//             Lorem ipsum... pretend more ...
-//           </CardText> */}
-//           <CardActions>
-//             <Button raised primary>Action 1</Button>
-//             <Button flat secondary>Action 2</Button>
-//           </CardActions>
-//         </Card>
-//       </div>
-//     );
-//   }
-// }
-
-// ReactDOM.render( <App />, document.getElementById('app'));
-
-import React from 'react'
-import ReactDOM from 'react-dom';
-import { Navbar, Button, Alignment } from "@blueprintjs/core";
+import * as React from 'react'
+import ReactDOM from 'react-dom'
+import { H3, H5, Switch, Classes, Navbar, Button, Alignment } from "@blueprintjs/core";
+import { Tab, Tabs } from "@blueprintjs/core";
+import { Example, handleBooleanChange, IExampleProps } from "@blueprintjs/docs-theme"
 
 import './app.scss'
 
+
 class App extends React.Component {
-    render() {
-      return (
-        <Navbar className="bp3-dark">
-            <Navbar.Group align={Alignment.LEFT}>
-                <Navbar.Heading>Blueprint</Navbar.Heading>
-                <Navbar.Divider />
-                <Button className="bp3-minimal" icon="home" text="Home" />
-                <Button className="bp3-minimal" icon="document" text="Files" />
-            </Navbar.Group>
+  handleNavbarTabChange(navbarTabId) {
+    this.setState({ navbarTabId })
+  } 
+
+  render() {
+    const options = (
+      <div>
+          <H5>Props</H5>
+      </div>
+   );
+
+    return (
+      <div id="navi" className="skaffe-main bp3-dark">
+        <Navbar>
+          <Navbar.Group align={Alignment.LEFT}>
+              <Navbar.Heading>Skaffe</Navbar.Heading>
+              <Navbar.Divider />
+              <Button className="bp3-minimal" icon="home" text="Home" />
+              <Button className="bp3-minimal" icon="document" text="Files" />
+          </Navbar.Group>
         </Navbar>        
-      );
-    }
+        <Tabs id="TabsExample" onChange={this.handleTabChange} selectedTabId="rx">
+          <Tab id="ng" title="Angular" panel={<AngularPanel />} />
+          <Tab id="mb" title="Ember" panel={<EmberPanel />} panelClassName="ember-panel" />
+          <Tab id="rx" title="React" panel={<ReactPanel />} />
+          <Tab id="bb" disabled title="Backbone" panel={<BackbonePanel />} />
+          <Tabs.Expander />
+          <input className="bp3-input" type="text" placeholder="Search..." />
+        </Tabs>
+      </div>
+    );
+  }
 }
+
+const ReactPanel = () => (
+  <div>
+      <H3>Example panel: React</H3>
+      <p className={Classes.RUNNING_TEXT}>
+          Lots of people use React as the V in MVC. Since React makes no assumptions about the rest of your technology
+          stack, it's easy to try it out on a small feature in an existing project.
+      </p>
+  </div>
+);
+
+const AngularPanel = () => (
+  <div>
+      <H3>Example panel: Angular</H3>
+      <p className={Classes.RUNNING_TEXT}>
+          HTML is great for declaring static documents, but it falters when we try to use it for declaring dynamic
+          views in web-applications. AngularJS lets you extend HTML vocabulary for your application. The resulting
+          environment is extraordinarily expressive, readable, and quick to develop.
+      </p>
+  </div>
+);
+
+const EmberPanel = () => (
+  <div>
+      <H3>Example panel: Ember</H3>
+      <p className={Classes.RUNNING_TEXT}>
+          Ember.js is an open-source JavaScript application framework, based on the model-view-controller (MVC)
+          pattern. It allows developers to create scalable single-page web applications by incorporating common idioms
+          and best practices into the framework. What is your favorite JS framework?
+      </p>
+      <input className={Classes.INPUT} type="text" />
+  </div>
+);
+
+const BackbonePanel = () => (
+  <div>
+      <H3>Backbone</H3>
+  </div>
+);
 
 ReactDOM.render( <App />, document.getElementById('app'));
